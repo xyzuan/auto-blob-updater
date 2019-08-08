bc9e62bbb6d7ec784d3bff393ee04d7ae88e3369
baalajimaestro <baalajimaestro@computer4u.com>
2019-08-08 10:14:46 +0530

    Rom-Fetcher: Inline with XiaomiUpdater JSON Changes
    We prefer China ROMs over global coz they release earlier
    
    Signed-off-by: baalajimaestro <baalajimaestro@computer4u.com>

--- get_rom.py
+++ get_rom.py
@@ -2,7 +2,7 @@ from requests import get
 from datetime import datetime as dt
 import json
 with open('violet_weekly.json', 'wb') as load:
-    load.write(get("https://raw.githubusercontent.com/XiaomiFirmwareUpdater/xiaomifirmwareupdater.github.io/master/data/devices/latest/weekly/violet.json").content)
+    load.write(get("https://raw.githubusercontent.com/XiaomiFirmwareUpdater/xiaomifirmwareupdater.github.io/master/data/devices/latest/violet.json").content)
 fw_weekly = json.loads(open('violet_weekly.json').read())
 weekly_date = dt.strptime(fw_weekly[1]["date"], "%Y-%m-%d")
 if weekly_date:
@@ -12,7 +12,7 @@ if weekly_date:
         load.write(str.encode(version))
     URL+=version
     URL+="/"
-    file=fw_weekly[1]["file"]
+    file=fw_weekly[1]["filename"]
     file=file[10:]
     URL+=file
     print("Fetching Weekly ROM......")
