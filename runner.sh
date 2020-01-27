b71b2b4aa4dc5ab188185335fba8867f889892e2
Dyneteve <dyneteve@pixelexperience.org>
2020-01-27 20:21:18 +0200

    CI: Don't install repo

--- runner.sh
+++ runner.sh
@@ -11,7 +11,7 @@
 
 echo "***Auto Blob Updater***"
 apt update > /dev/null 2>&1
-apt install curl git python3 python3-pip patchelf brotli unzip zip repo p7zip-full -y > /dev/null 2>&1
+apt install curl git python3 python3-pip patchelf brotli unzip zip p7zip-full -y > /dev/null 2>&1
 pip3 install requests > /dev/null 2>&1
 python3 strip.py
 bash -c "bash runner_user.sh"
