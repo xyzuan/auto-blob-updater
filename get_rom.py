b112af2575446f0ed80530c140e25dd8fc9a281a
baalajimaestro <baalajimaestro@pixelexperience.org>
2020-01-28 22:35:28 +0530

    fetcher: Inline with xiaomi updater changes
    Signed-off-by: baalajimaestro <baalajimaestro@pixelexperience.org>

--- get_rom.py
+++ get_rom.py
@@ -1,18 +1,31 @@
 from requests import get
 from datetime import datetime as dt
-import json
-with open('violet_weekly.json', 'wb') as load:
-    load.write(get("https://raw.githubusercontent.com/XiaomiFirmwareUpdater/xiaomifirmwareupdater.github.io/master/data/devices/latest/violet.json").content)
-fw_weekly = json.loads(open('violet_weekly.json').read())
+import yaml
+
+with open('violet.yml', 'wb') as load:
+    load.write(get("https://raw.githubusercontent.com/XiaomiFirmwareUpdater/xiaomifirmwareupdater.github.io/master/data/devices/latest/violet.yml").content)
+fw = yaml.safe_load(open('violet.yml').read())
+stable_date = dt.strptime(fw[1]["date"], "%Y-%m-%d")
 weekly_date = dt.strptime(fw_weekly[1]["date"], "%Y-%m-%d")
-if weekly_date:
+if stable_date > weekly_date:
     URL="https://bigota.d.miui.com/"
     version=fw_weekly[1]["versions"]["miui"]
     with open('/tmp/version','wb') as load:
         load.write(str.encode(version))
     URL+=version
     URL+="/"
-    file=fw_weekly[1]["filename"]
+    file=fw[1]["filename"]
+    file=file[10:]
+    URL+=file
+    print("Fetching Stable ROM......")
+else:
+    URL="https://bigota.d.miui.com/"
+    version=fw[3]["versions"]["miui"]
+    with open('/tmp/version','wb') as load:
+        load.write(str.encode(version))
+    URL+=version
+    URL+="/"
+    file=fw[3]["filename"]
     file=file[10:]
     URL+=file
     print("Fetching Weekly ROM......")
